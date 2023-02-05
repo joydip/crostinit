f640f85e0e5a390f8ca597187102e9d466964c11
Joydip Basu <joydawg@gmail.com>
2023-02-05 14:03:38 +0000

    Update visual-studio-code.sh

--- visual-studio-code.sh
+++ visual-studio-code.sh
@@ -1,2 +1,4 @@
 # Visual Studio Code
-curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o /tmp/code.deb
+sudo apt install -y /tmp/code.deb
+rm /tmp/code.deb
