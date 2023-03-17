01d4807c820d6a03c0ecfcc7533c2d5725c236a6
Joydip Basu <joydawg@gmail.com>
2023-03-17 14:09:05 +0000

    Remove hardcoded architecture

--- visual-studio-code.sh
+++ visual-studio-code.sh
@@ -1,4 +1,4 @@
 # Visual Studio Code
-curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o /tmp/code.deb
+curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-$(dpkg --print-architecture)" -o /tmp/code.deb
 sudo apt install -y /tmp/code.deb
 rm /tmp/code.deb
