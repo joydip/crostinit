a4900a05c1463ad306f8f581db70460b05a26099
Joydip Basu <joydawg@gmail.com>
2024-10-13 14:01:48 +0100

    Update version and make arch dynamic

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
 # go
-curl -L "https://go.dev/dl/go1.19.4.linux-amd64.tar.gz" | sudo tar -C /usr/local -xzv
+curl -L "https://go.dev/dl/go1.23.2.linux-$(dpkg --print-architecture).tar.gz" | sudo tar -C /usr/local -xzv
 export PATH=$PATH:/usr/local/go/bin
