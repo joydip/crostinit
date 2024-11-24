1a69e6e4b6f5e0e0083fc1a69d30928bac172080
Joydip Basu <joydawg@gmail.com>
2024-11-24 15:08:58 +0000

    Fix dynamic architecture value

--- visual-studio-code.sh
+++ visual-studio-code.sh
@@ -1,4 +1,10 @@
 # Visual Studio Code
+
+ARCH=$(dpkg --print-architecture)
+if [ "$ARCH" == "amd64" ]; then
+    ARCH="x64"
+fi
+
 curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o /tmp/code.deb
 sudo apt install -y /tmp/code.deb
 rm /tmp/code.deb
