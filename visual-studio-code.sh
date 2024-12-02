8020feec71e676e12f68fb4d88bf8513323f3b9f
Joydip Basu <joydawg@gmail.com>
2024-12-02 11:39:02 +0000

    Fix missing arch interpolation

--- visual-studio-code.sh
+++ visual-studio-code.sh
@@ -5,6 +5,6 @@ if [ "$ARCH" == "amd64" ]; then
     ARCH="x64"
 fi
 
-curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o /tmp/code.deb
+curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-$ARCH" -o /tmp/code.deb
 sudo apt install -y /tmp/code.deb
 rm /tmp/code.deb
