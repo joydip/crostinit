e2d919f35d7c95d3b2f1e07e83d0509c168047c3
Joydip Basu <joydawg@gmail.com>
2024-12-02 11:51:39 +0000

    Fetch latest version and removing arch hardcoding

--- restic.sh
+++ restic.sh
@@ -1,4 +1,8 @@
 # restic
-curl -Lo - https://github.com/restic/restic/releases/download/v0.15.2/restic_0.15.2_linux_$(dpkg --print-architecture).bz2 | bunzip2 | sudo tee /usr/local/bin/restic >/dev/null
+ARCH=$(dpkg --print-architecture)
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/restic/restic/releases/latest | grep 'tag_name' | cut -d\" -f4)
+DOWNLOAD_URL="https://github.com/restic/restic/releases/download/${LATEST_RELEASE}/restic_${LATEST_RELEASE:1}_linux_${ARCH}.bz2"
+echo $DOWNLOAD_URL
+curl -Lo - $DOWNLOAD_URL | bunzip2 | sudo tee /usr/local/bin/restic >/dev/null
 sudo chmod a+x /usr/local/bin/restic
-sudo restic self-update
+
