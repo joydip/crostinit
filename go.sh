95c3886c9f8f418ac509c59bbc3a1f3f26295b97
Joydip Basu <joydawg@gmail.com>
2024-12-08 09:28:16 -0800

    Remove hardcoded version and arch

--- go.sh
+++ go.sh
@@ -1,3 +1,24 @@
 # go
-curl -L "https://go.dev/dl/go1.23.2.linux-$(dpkg --print-architecture).tar.gz" | sudo tar -C /usr/local -xzv
+VERSION=$(curl -L https://go.dev/VERSION?m=text)
+ARCH=$(dpkg --print-architecture)
+case $ARCH in
+  amd64)
+    GOARCH="amd64"
+    ;;
+  arm64)
+    GOARCH="arm64"
+    ;;
+  armhf)
+    GOARCH="armv6l"
+    ;;
+  *)
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+    ;;
+esac
+  
+URL="https://go.dev/dl/${VERSION}.linux-${GOARCH}.tar.gz"
+sudo rm -rf /usr/local/go
+curl -L "$URL" | sudo tar -C /usr/local -xzv
 export PATH=$PATH:/usr/local/go/bin
+go version
